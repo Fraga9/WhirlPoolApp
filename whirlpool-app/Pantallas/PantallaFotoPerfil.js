8090338394f8735c25de7b325cf1afe1bf8d3cc3
Fraga9 <a00835812@tec.mx>
2024-04-24 12:46:47 -0600

    pantallafoto

--- whirlpool-app/Pantallas/PantallaFotoPerfil.js
+++ whirlpool-app/Pantallas/PantallaFotoPerfil.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import { Text, View, TouchableOpacity, Alert, StyleSheet, Image } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import axios from 'axios';
 
 const PantallaFotoPerfil = () => {
     const [profileImage, setProfileImage] = useState(null);
 
     const loadImageFromGallery = async () => {
         const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        console.log("Estado de los permisos de la galería:", status)
 
         if (status !== 'granted') {
             Alert.alert("Debes dar permiso para acceder a las imágenes del dispositivo.");
@@ -16,13 +18,37 @@ const PantallaFotoPerfil = () => {
         const result = await ImagePicker.launchImageLibraryAsync({
             allowsEditing: true,
             aspect: [1, 1],
+            quality: 1,
         });
 
-        if (result.canceled) {
+        console.log("Resultado de la selección de imagen:", result);
+
+        if (result.cancelled || !result.uri || !result.assets[0].uri) {
+            console.log("Selección de imagen cancelada.");
             return;
         }
 
+        console.log("URI de la imagen seleccionada:", result.uri);
+
         setProfileImage(result.uri);
+
+        // Crear FormData para enviar la imagen al servidor
+        let formData = new FormData();
+        formData.append('file', {
+            uri: result.assets[0].uri,
+            type: 'image/jpeg',
+            name: 'profile_photo.jpg',
+        });
+
+        // Enviar la imagen al servidor
+        console.log("Enviando imagen al servidor...");
+        axios.post('http://54.86.33.126:8000/reportes/foto/', formData)
+            .then(response => {
+                console.log('Imagen enviada al servidor:', response.data);
+            })
+            .catch(error => {
+                console.error('Error al enviar la imagen al servidor:', error);
+            });
     };
 
     return (
@@ -33,11 +59,8 @@ const PantallaFotoPerfil = () => {
                 <Image style={styles.foto} source={profileImage ? { uri: profileImage } : require("../images/iconoempleado.png")} />
             </View>
 
-            <TouchableOpacity style={[styles.button, { backgroundColor: '#D9D9D9', marginTop: 60 }]}>
-                <Text style={styles.texto}>Tomar foto y subir</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={[styles.button, { backgroundColor: '#000000' }]} onPress={loadImageFromGallery}>
-                <Text style={[styles.texto, { color: '#fff' }]}>Escoger del dispositivo</Text>
+            <TouchableOpacity style={[styles.button, { backgroundColor: '#D9D9D9', marginTop: 60 }]} onPress={loadImageFromGallery}>
+                <Text style={styles.texto}>Seleccionar desde la galería</Text>
             </TouchableOpacity>
         </View>
     );
