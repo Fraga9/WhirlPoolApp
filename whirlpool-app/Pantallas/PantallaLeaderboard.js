c9ecca2945e7b6a2e2934f28f940c96e07ee633c
Fraga9 <A00835812@tec.mx>
2024-04-23 23:04:17 -0600

    leaderboard

--- whirlpool-app/Pantallas/PantallaLeaderboard.js
+++ whirlpool-app/Pantallas/PantallaLeaderboard.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { ScrollView, View, Text, StyleSheet, ActivityIndicator } from 'react-native';
 import { ListItem, Avatar } from 'react-native-elements';
+import { Ionicons } from '@expo/vector-icons';
 import axios from 'axios';
 
 const Leaderboard = () => {
@@ -10,7 +11,7 @@ const Leaderboard = () => {
   useEffect(() => {
     axios.get('http://54.86.33.126:8000/reportes/empleado')
       .then(response => {
-        const sortedEmpleados = response.data.sort((a, b) => b.puntos_juego - a.puntos_juego);
+        const sortedEmpleados = response.data.sort((a, b) => b.puntos_trabajo - a.puntos_trabajo);
         setEmpleados(sortedEmpleados);
         setLoading(false);
       })
@@ -29,28 +30,55 @@ const Leaderboard = () => {
           <View style={styles.podium}>
             <View style={styles.podiumItem}>
               <Avatar source={{ uri: empleados[2]?.foto_perfil }} />
-              <Text>{empleados[2]?.nombre + ' ' + empleados[2]?.apellido}</Text>
-              <Text>3rd</Text>
+              <Text style={{ color: '#FFFFFF', marginTop: 5, fontWeight: 'bold' }}>{empleados[2]?.nombre + ' ' + empleados[2]?.apellido}</Text>
+              <View style={styles.points}>
+                <Ionicons name="star-outline" size={16} color="white" />
+                <Text style={{ color: '#FFFFFF', fontWeight: 'bold', fontSize: 16, marginLeft: 10 }}>{empleados[2].puntos_trabajo}</Text>
+              </View>
+              <View style={styles.bar3}>
+                <Text style={styles.barChar}>3</Text>
+              </View>
             </View>
             <View style={styles.podiumItem}>
               <Avatar source={{ uri: empleados[0]?.foto_perfil }} />
-              <Text>{empleados[0]?.nombre + ' ' + empleados[0]?.apellido}</Text>
-              <Text>1st</Text>
+              <Text style={{ color: '#FFFFFF', marginTop: 5, fontWeight: 'bold' }}>{empleados[0]?.nombre + ' ' + empleados[0]?.apellido}</Text>
+              <View style={styles.points}>
+                <Ionicons name="star-outline" size={16} color="white" />
+                <Text style={{ color: '#FFFFFF', fontWeight: 'bold', fontSize: 16, marginLeft: 10 }}>{empleados[0].puntos_trabajo}</Text>
+              </View>
+              <View style={styles.bar1}>
+                <Text style={styles.barChar}>1</Text>
+              </View>
             </View>
             <View style={styles.podiumItem}>
               <Avatar source={{ uri: empleados[1]?.foto_perfil }} />
-              <Text>{empleados[1]?.nombre + ' ' + empleados[1]?.apellido}</Text>
-              <Text>2nd</Text>
+              <Text style={{ color: '#FFFFFF', marginTop: 5, fontWeight: 'bold' }}>{empleados[1]?.nombre + ' ' + empleados[1]?.apellido}</Text>
+              <View style={styles.points}>
+                <Ionicons name="star-outline" size={16} color="white" />
+                <Text style={{ color: '#FFFFFF', fontWeight: 'bold', fontSize: 16, marginLeft: 10 }}>{empleados[1].puntos_trabajo}</Text>
+              </View>
+              <View style={styles.bar2}>
+                <Text style={styles.barChar}>2</Text>
+              </View>
             </View>
           </View>
-          {empleados.slice(3).map((empleado, i) => (
-            <ListItem key={i} bottomDivider>
-              <ListItem.Content>
-                <ListItem.Title>{i+4}. {empleado.nombre + ' ' + empleado.apellido}</ListItem.Title>
-                <ListItem.Subtitle>{empleado.puntos_juego} puntos</ListItem.Subtitle>
-              </ListItem.Content>
-            </ListItem>
-          ))}
+          <View style={styles.underPodium}>
+            {empleados.slice(3).map((empleado, index) => (
+              <ListItem key={index} containerStyle={styles.listItemContainer}>
+                <ListItem.Content style={styles.listItemContent}>
+                  <View style={styles.listItemLeft}>
+                    <Text style={styles.listItemPosition}>{index + 4}</Text>
+                    <Avatar source={{ uri: empleado.foto_perfil }} />
+                    <ListItem.Title style={styles.listItemTitle}>{empleado.nombre + ' ' + empleado.apellido}</ListItem.Title>
+                  </View>
+                  <View style={styles.points}>
+                    <Ionicons name="star-outline" size={16} color="white" />
+                    <Text style={{ color: '#FFFFFF', fontWeight: 'bold', fontSize: 16, marginLeft: 10 }}>{empleado.puntos_trabajo}</Text>
+                  </View>
+                </ListItem.Content>
+              </ListItem>
+            ))}
+          </View>
         </View>
       )}
     </ScrollView>
@@ -65,22 +93,101 @@ const styles = StyleSheet.create({
   },
   leaderboardContainer: {
     flex: 1,
-    padding: 20,
+    backgroundColor: '#141110',
+
   },
   leaderboardTitle: {
     fontSize: 24,
     fontWeight: 'bold',
+    color: '#FFF6EA',
     textAlign: 'center',
     marginBottom: 20,
+    marginTop: 20,
   },
   podium: {
     flexDirection: 'row',
     justifyContent: 'space-around',
-    marginBottom: 20,
+    height: 250,
+    padding: 10,
   },
   podiumItem: {
     alignItems: 'center',
   },
+  underPodium: {
+    backgroundColor: '#FFF6EA',
+    flex: 1,
+    height: 800,
+    borderRadius: 30,
+    zIndex: 2,
+  },
+
+  bar1: {
+    backgroundColor: '#eeb111',
+    width: 100,
+    height: 150,
+    borderRadius: 10,
+    marginTop: 40,
+  },
+  bar2: {
+    backgroundColor: '#eeb111',
+    width: 100,
+    height: 150,
+    borderRadius: 10,
+    marginTop: 60,
+  },
+  bar3: {
+    backgroundColor: '#eeb111',
+    width: 100,
+    height: 150,
+    borderRadius: 10,
+    marginTop: 80,
+  },
+
+  barChar: {
+    color: '#FFFFFF',
+    fontSize: 32,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    marginTop: 10,
+  },
+
+  points: {
+    backgroundColor: '#eeb111',
+    width: 90,
+    height: 30,
+    borderRadius: 30,
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  listItemContainer: {
+    backgroundColor: '#FFF6EA',
+    marginTop: 10,
+    borderRadius: 10,
+    marginHorizontal: 10,
+  },
+  listItemContent: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  listItemLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  listItemPosition: {
+    marginRight: 10,
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  listItemTitle: {
+    marginLeft: 10,
+    fontSize: 16,
+  },
+
+
 });
 
 export default Leaderboard;
