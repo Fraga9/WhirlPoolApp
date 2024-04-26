c634663162c442737af509d5690bdbb85ddc076d
Fraga9 <a00835812@tec.mx>
2024-04-26 08:42:21 -0600

    PantallaFotoPerfil modificaciones

--- whirlpool-app/Pantallas/PantallaFotoPerfil.js
+++ whirlpool-app/Pantallas/PantallaFotoPerfil.js
@@ -46,7 +46,7 @@ const PantallaFotoPerfil = () => {
 
         console.log("URI de la imagen seleccionada:", result.assets[0].uri);
 
-        setProfileImage(result.assets[0].uri);
+        setProfileImage({ uri: result.assets[0].uri });
     };
 
     const updateProfilePicture = async () => {
@@ -61,34 +61,52 @@ const PantallaFotoPerfil = () => {
         // The URL of the API endpoint
         const url = `http://54.86.33.126:8000/reportes/empleado/${employeeId}/`;
 
+        // Convert the image URI to a blob
+        const response = await fetch(profileImage.uri);
+        const blob = await response.blob();
+
+        console.log("Blob:", blob);
+
         // Create a new FormData instance
         let formData = new FormData();
 
-        // Get the local file as a blob
-        let localUri = profileImage;
-        let filename = localUri.split('/').pop();
+        // Define the filename
+        const filename = profileImage.uri.split('/').pop();
 
-        // Infer the type of the image
-        let match = /\.(\w+)$/.exec(filename);
-        let type = match ? `image/${match[1]}` : `image`;
+        let file = new File([blob], filename, { type: 'image/png' });
+
+        console.log("File:", file);
 
         // Add the image to the form data
-        console.log("Adding image to form data:", { uri: localUri, name: filename, type });
-        formData.append('foto_perfil', { uri: localUri, name: filename, type });
+        console.log("Adding image to form data:", { uri: profileImage.uri, name: filename, type: 'image/jpeg' });
+        formData.append('foto_perfil', file, 'nombre_archivo.png');
 
-        fetch(url, {
-            method: 'PATCH',
-            body: formData,
+        axios({
+            method: 'patch',
+            url: url,
+            data: formData,
             headers: {
                 'Content-Type': 'multipart/form-data',
             },
         })
-            .then(response => response.json())
-            .then(data => {
-                console.log('Employee updated:', data);
+            .then((response) => {
+                console.log('Employee updated:', response.data);
+                setProfileImage({ uri: response.data.foto_perfil });
             })
-            .catch(error => {
+            .catch((error) => {
                 console.error('Error updating employee:', error);
+                if (error.response) {
+                    console.log('Response data:', error.response.data);
+                    console.log('Response status:', error.response.status);
+                    console.log('Response headers:', error.response.headers);
+                }
+                else if (error.request) {
+                    console.log('Request:', error.request);
+                }
+                else {
+                    console.log('Error:', error.message);
+                }
+                console.log('Error config:', error.config);
             });
     };
 
@@ -98,7 +116,7 @@ const PantallaFotoPerfil = () => {
 
             <View style={styles.circle}>
                 {profileImage ? (
-                    <Image style={styles.foto} source={{ uri: profileImage }} />
+                    <Image style={styles.foto} source={{ uri: profileImage.uri }} />
                 ) : (
                     <Image style={styles.foto} source={require("../images/iconoempleado.png")} />
                 )}
