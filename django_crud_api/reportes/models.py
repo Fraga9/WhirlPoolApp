fdfe913cf783b7eafb7e1f2b53fd738316d7794f
Fraga9 <A00835812@tec.mx>
2024-04-28 20:46:09 -0600

    Test true

--- django_crud_api/reportes/models.py
+++ django_crud_api/reportes/models.py
@@ -73,7 +73,7 @@ class Personaje(models.Model):
 
 
 class EmpleadoPersonaje(models.Model):
-    id_emp = models.AutoField(primary_key=True)
+    id_emp = models.AutoField(primary_key=True, null=True)
     empleado = models.ForeignKey(Empleado, on_delete=models.CASCADE)
     personaje = models.ForeignKey(Personaje, on_delete=models.CASCADE)
     nivel_actual = models.IntegerField()
